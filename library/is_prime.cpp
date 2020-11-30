a45b714cae411c92a3802757cd2d70216cfa3494
pippi-sniper <67419268+pippi-sniper@users.noreply.github.com>
2020-11-30 15:27:12 +0900

    Update is_prime.cpp

--- library/is_prime.cpp
+++ library/is_prime.cpp
@@ -2,7 +2,6 @@ bool is_prime(int n){
     if (n < 2) return 0;
     else if (n == 2) return 1;
     else if (n % 2 == 0) return 0;
-    double sqrtN = sqrt(n);
-    for (int i = 3; i <= sqrtN; i += 2) if (n % i == 0) return 0;
+    for(int i = 3; i <= (double)n; i += 2) if(n % i == 0) return 0;
     return 1;
 }
