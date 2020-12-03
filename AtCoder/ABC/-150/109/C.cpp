d0405be20417938b32c2b6e8bd6f0489964e6bf6
pippi-sniper <67419268+pippi-sniper@users.noreply.github.com>
2020-12-03 14:27:02 +0900

    Update C.cpp

--- AtCoder/ABC/-150/109/C.cpp
+++ AtCoder/ABC/-150/109/C.cpp
@@ -1,72 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
-int scan() { return getchar(); }
-void scan(int &a) { cin >> a; }
-void scan(long long &a) { cin >> a; }
-void scan(char &a) { cin >> a; }
-void scan(double &a) { cin >> a; }
-void scan(string &a) { cin >> a; }
-template <class T, class S> void scan(pair<T, S> &p) { scan(p.first), scan(p.second); }
-template <class T> void scan(vector<T> &);
-template <class T> void scan(vector<T> &a) {
-    for(auto &i : a) scan(i);
-}
-template <class T> void scan(T &a) { cin >> a; }
-void IN() {}
-template <class Head, class... Tail> void IN(Head &head, Tail &... tail) {
-    scan(head);
-    IN(tail...);
-}
-#define all(a) a.begin(),a.end() 
-#define Srep(n) for(int64_t i = 0; i < int64_t(n); i++)
-#define Lrep(i,a,n) for(int64_t i = int64_t(a); i < int64_t(n); i++)
+const double pi = 3.14159265358979;
+#define setup\
+    cin.tie(0);\
+    ios::sync_with_stdio(false);\
+    cout << setprecision(20) << fixed;
+#define int long long int
+#define db double
+#define P pair<int,int>
+#define F first
+#define S second
+#define dtor(deg) (((deg)/360)*2*pi)
+#define rtod(rad) (((rad)/2/pi)*360)
+#define all(a) a.begin(),a.end()
+#define Srep(n) for(int i = 0; i < n; i++)
+#define Lrep(i,a,n) for(int i = a; i < n; i++)
 #define Brep1st(n) for(int bit = 0; bit < (1 << n); bit++)
 #define Brep2nd(n) Srep(n) if(bit >> i & 1)
 #define rep2d(n,m) Srep(n) Lrep(j,0,m)
-#define vec(type,name) vector<type> name
-#define vecc(type,name,...) vector<type> name(__VA_ARGS__)
-#define vvec(type,name,size,...) vector<vector<type>> name(size, vector<type>(__VA_ARGS__))
-#define scan(...) IN(__VA_ARGS__)
-#define intscan(...)\
-    int __VA_ARGS__;\
-    IN(__VA_ARGS__)
-#define i64scan(...)\
-    int64_t __VA_ARGS__;\
-    IN(__VA_ARGS__)
-#define charscan(...)\
-    char __VA_ARGS__;\
-    IN(__VA_ARGS__)
-#define strscan(...)\
-    string __VA_ARGS__;\
-    IN(__VA_ARGS__)
-#define vecscan(type,name,size)\
-    vector<type> name(size);\
-    IN(name)
-#define vvecscan(type,name,size1,size2)\
-    vector<vector<type>> name(size1, vector<type>(size2));\
-    IN(name)
-#define printvecs(name)\
-    Srep(name.size()-1) cout << name[i] << ' ';\
-    cout << name[name.size()-1] << endl;
-#define printvece(name)\
-    Srep(name.size()) cout << name[i] << endl;
-#define printvvec(name)\
-    Srep(name.size()){\
-        Lrep(j,0,name[i].size()-1) cout << name[i][j] << ' ';\
-        cout << name[i][name[i].size()-1] << endl;\
-    }
+#define vi vector<int>
+#define vvi vector<vi>
+#define vc vector<char>
+#define vvc vector<vc>
 
-int main(){
-    intscan(n);
-    set<string> j;
-    vecc(string,input,n);
-    bool ans = 1;
-    Srep(n){
-        scan(input[i]);
-        auto ok = j.insert(input[i]);
-        if(!ok.second) ans = 0;
-        if(i == 0) continue;
-        if(input[i][0] != input[i-1][input[i-1].size()-1]) ans = 0;
+int gcd(int a, int b) { return b ? gcd(b, a % b) : a; }
+
+signed main(){
+    setup;
+    int n, x, ans;
+    cin >> n >> x >> ans;
+    ans = abs(ans - x);
+    Srep(n-1){
+        int input;
+        cin >> input;
+        input = abs(input - x);
+        ans = gcd(ans, input);
     }
-    cout << (ans ? "Yes" : "No") << endl;
+    cout << ans << endl;
 }
