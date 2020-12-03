de079f2dcd722a0e82e910e62290d6b4024fd8da
pippi-sniper <67419268+pippi-sniper@users.noreply.github.com>
2020-12-03 09:09:57 +0900

    Create A.cpp

---
+++ AtCoder/AGC/002/A.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+const double pi = 3.14159265358979;
+#define setup\
+    cin.tie(0);\
+    ios::sync_with_stdio(false);\
+    cout << setprecision(20) << fixed;
+#define int long long int
+#define db double
+#define P pair<int,int>
+#define F first
+#define S second
+#define dtor(deg) (((deg)/360)*2*pi)
+#define rtod(rad) (((rad)/2/pi)*360)
+#define all(a) a.begin(),a.end()
+#define Srep(n) for(int i = 0; i < n; i++)
+#define Lrep(i,a,n) for(int i = a; i < n; i++)
+#define Brep1st(n) for(int bit = 0; bit < (1 << n); bit++)
+#define Brep2nd(n) Srep(n) if(bit >> i & 1)
+#define rep2d(n,m) Srep(n) Lrep(j,0,m)
+#define vi vector<int>
+#define vvi vector<vi>
+#define vc vector<char>
+#define vvc vector<vc>
+
+signed main(){
+    setup;
+    int a, b;
+    cin >> a >> b;
+    if(0 < a) cout << "Positive" << endl;
+    else if(b < 0){
+        if(abs(a - b) % 2) cout << "Positive" << endl;
+        else cout << "Negative" << endl;
+    }else cout << "Zero" << endl;
+}
