b28cfd1cdeb0635510d93ef1d57b5b59ecbef8e8
pippi-sniper <67419268+pippi-sniper@users.noreply.github.com>
2020-12-04 14:01:09 +0900

    Update and rename is_prime.cpp to prime.cpp

--- library/is_prime.cpp
+++
@@ -1,7 +0,0 @@
-bool is_prime(int n){
-    if (n < 2) return 0;
-    else if (n == 2) return 1;
-    else if (n % 2 == 0) return 0;
-    for(int i = 3; i <= (double)sqrt(n); i += 2) if(n % i == 0) return 0;
-    return 1;
-}
---
+++ library/prime.cpp
@@ -0,0 +1,20 @@
+bool is_prime(int n){
+    if (n < 2) return 0;
+    else if (n == 2) return 1;
+    else if (n % 2 == 0) return 0;
+    for(int i = 3; i * i <= n; i += 2) if(n % i == 0) return 0;
+    return 1;
+}
+
+vector<bool> prime_list(int n){
+    vector<bool> prime(n+1, 1);
+    if(n >= 0) prime[0] = 0;
+    if(n >= 1) prime[1] = 0;
+    for(int i = 2; i * i <= n; i++){
+        if(!prime[i]) continue;
+        for(int j = i * i; j <= n; j += i){
+            prime[j] = 0;
+        }
+    }
+    return prime;
+}
