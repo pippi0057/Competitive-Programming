69e10c68fe1130a8778cfff3eba581306143d86a
pippi-sniper <67419268+pippi-sniper@users.noreply.github.com>
2020-12-05 09:51:52 +0900

    Update C.cpp

--- AtCoder/ABC/-180/166/C.cpp
+++ AtCoder/ABC/-180/166/C.cpp
@@ -1,9 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 const double pi = 3.14159265358979;
+#define setup\
+    cin.tie(0);\
+    ios::sync_with_stdio(false);\
+    cout << setprecision(20) << fixed;
 #define int long long int
 #define db double
-#define ldb long double
 #define P pair<int,int>
 #define F first
 #define S second
@@ -17,13 +20,23 @@ const double pi = 3.14159265358979;
 #define rep2d(n,m) Srep(n) Lrep(j,0,m)
 #define vi vector<int>
 #define vvi vector<vi>
-#define vc vector<char>
-#define vvc vector<vc>
+#define vb vector<bool>
 
-int32_t main(){
-    int n,m;
+signed main(){
+    setup;
+    int n, m, ans = 0;
     cin >> n >> m;
     vi h(n);
+    vb s(n, 1);
     Srep(n) cin >> h[i];
-    
+    Srep(m){
+        int a, b;
+        cin >> a >> b;
+        a--; b--;
+        if(h[a] < h[b]) s[a] = 0;
+        else if(h[a] > h[b]) s[b] = 0;
+        else s[a] = s[b] = 0;
+    }
+    Srep(n) if(s[i]) ans++;
+    cout << ans << endl;
 }
