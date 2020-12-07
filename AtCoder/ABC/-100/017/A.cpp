b9d01b21614d0d0c2461e9fc4d8f40256a4cea23
pippi-sniper <67419268+pippi-sniper@users.noreply.github.com>
2020-12-07 18:23:19 +0900

    Create A.cpp

---
+++ AtCoder/ABC/-100/017/A.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+const double pi = 3.14159265358979;
+#define setup\
+    cin.tie(0);\
+    ios::sync_with_stdio(false);\
+    cout << setprecision(20) << fixed;
+#define int long long int
+#define db double
+#define P pair<int,int>
+#define F first
+#define S second
+#define endl "\n"
+#define dtor(deg) (((deg)/360)*2*pi)
+#define rtod(rad) (((rad)/2/pi)*360)
+#define all(a) a.begin(),a.end()
+#define Srep(n) for(int i = 0; i < n; i++)
+#define Lrep(i,a,n) for(int i = a; i < n; i++)
+#define Brep1st(n) for(int bit = 0; bit < (1 << n); bit++)
+#define Brep2nd(n) Srep(n) if(bit >> i & 1)
+#define rep2d(n,m) Srep(n) Lrep(j,0,m)
+#define vi vector<int>
+#define vvi vector<vi>
+#define vdb vector<db>
+#define vb vector<bool>
+
+signed main(){
+    setup;
+    int ans = 0;
+    Srep(3){
+        int a, b;
+        cin >> a >> b;
+        ans += a * b / 10;
+    }
+    cout << ans << endl;
+}
