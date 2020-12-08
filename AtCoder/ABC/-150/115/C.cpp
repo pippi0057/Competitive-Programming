8170e62cf84d0085727b70eb4b69954884137be9
pippi-sniper <67419268+pippi-sniper@users.noreply.github.com>
2020-12-08 18:59:19 +0900

    Create C.cpp

---
+++ AtCoder/ABC/-150/115/C.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+const double pi = 3.14159265358979;
+#define setup\
+    cin.tie(0);\
+    ios::sync_with_stdio(false);\
+    cout << setprecision(20) << fixed;
+#define int long long int
+#define db double
+#define P pair<int,int>
+#define F first
+#define S second
+#define endl "\n"
+#define dtor(deg) (((deg)/360)*2*pi)
+#define rtod(rad) (((rad)/2/pi)*360)
+#define all(a) a.begin(),a.end()
+#define Srep(n) for(int i = 0; i < n; i++)
+#define Lrep(i,a,n) for(int i = a; i < n; i++)
+#define Brep1st(n) for(int bit = 0; bit < (1 << n); bit++)
+#define Brep2nd(n) Srep(n) if(bit >> i & 1)
+#define rep2d(n,m) Srep(n) Lrep(j,0,m)
+#define vi vector<int>
+#define vvi vector<vi>
+#define vdb vector<db>
+#define vb vector<bool>
+
+signed main(){
+    setup;
+    int n, k, ans = 1e10;
+    cin >> n >> k;
+    vi h(n);
+    Srep(n) cin >> h[i];
+    sort(all(h));
+    Srep(n-k+1) ans = min(ans, h[i+k-1] - h[i]);
+    cout << ans << endl;
+}
