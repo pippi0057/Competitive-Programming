3226aabc493dbe4e527c72a9b40bd850ec479d68
pippi-sniper <poyattosannokeitai@gmail.com>
2020-12-12 20:00:12 +0900

    D.cpp

---
+++ AtCoder/Others/三井住友 2019/D.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+const double pi = 3.14159265358979;
+const int inf = 1e13;
+#define setup cin.tie(0); ios::sync_with_stdio(false); cout << setprecision(20) << fixed;
+#define db double
+#define P pair<int,int>
+#define F first
+#define S second
+#define endl "\n"
+#define dtor(deg) (((deg)/360)*2*pi)
+#define rtod(rad) (((rad)/2/pi)*360)
+#define all(a) a.begin(),a.end()
+#define Srep(n) for(int i = 0; i < n; i++)
+#define Lrep(i,a,n) for(int i = a; i < n; i++)
+#define Brep1st(n) for(int bit = 0; bit < (1 << n); bit++)
+#define Brep2nd(n) Srep(n) if(bit >> i & 1)
+#define rep2d(n,m) Srep(n) Lrep(j,0,m)
+#define RoundUp(a,b) ((a) % (b) ? (a) / (b) + 1 : (a) / (b))
+#define vi vector<int>
+#define vvi vector<vi>
+#define vdb vector<db>
+#define vb vector<bool>
+#define V vector
+
+int factorial(int n){ return (n == 1 ? 1 : n * factorial(n-1)); }
+
+signed main(){
+    setup;
+    int n;
+    string s;
+    cin >> n;
+    vi s(n);
+    Srep(n){
+        char c;
+        cin >> c;
+        s[i] = c - '0';
+    }
+    vb _1(10);
+    V<V<vb>> dp(10, V<vb>(10, vb(10, 0)));
+    Srep(n){
+        
+        _1[s[i]] = 1;
+    }
+}
