9a6b006a9f421df546d747591f676f4b5a5db9be
pippi-sniper <poyattosannokeitai@gmail.com>
2021-01-04 20:46:40 +0900

    template

--- library/C++/template.cpp
+++ library/C++/template.cpp
@@ -7,12 +7,12 @@ const int inf = 1e13;
 #define endl "\n"
 #define dtor(deg) (((deg)/360)*2*pi)
 #define all(a) a.begin(),a.end()
-#define overload4(_1,_2,_3,_4,name,...) name
+#define overload(_1,_2,_3,_4,name,...) name
 #define rep1(n) for(int i = 0; i < (n); i++)
 #define rep2(i,n) for(int i = 0; i < (n); i++)
 #define rep3(i,a,b) for(int i = (a); i < (b); i++)
 #define rep4(i,a,b,c) for(int i = (a); i < (b); i += (c))
-#define rep(...) overload4(__VA_ARGS__,rep4,rep3,rep2,rep1)(__VA_ARGS__)
+#define rep(...) overload(__VA_ARGS__,rep4,rep3,rep2,rep1)(__VA_ARGS__)
 #define vec(type,name,...) vector<type> name(__VA_ARGS__)
 
 void Main(){
