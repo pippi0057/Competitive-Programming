096538adc75d2186d4ee1a94260257fe288f5ec5
pippi-sniper <poyattosannokeitai@gmail.com>
2021-01-04 20:39:08 +0900

    template

--- library/C++/template.cpp
+++ library/C++/template.cpp
@@ -3,24 +3,25 @@ using namespace std;
 #define int long long int
 const double pi = 3.14159265358979;
 const int inf = 1e13;
-#define setup cin.tie(0); ios::sync_with_stdio(false); cout << setprecision(20) << fixed;
 #define db double
-#define P pair<int,int>
-#define F first
-#define S second
 #define endl "\n"
 #define dtor(deg) (((deg)/360)*2*pi)
-#define rtod(rad) (((rad)/2/pi)*360)
 #define all(a) a.begin(),a.end()
-#define Srep(n) for(int i = 0; i < n; i++)
-#define Lrep(i,a,n) for(int i = a; i < n; i++)
-#define Brep1st(n) for(int bit = 0; bit < (1 << n); bit++)
-#define Brep2nd(n) Srep(n) if(bit >> i & 1)
-#define rep2d(n,m) Srep(n) Lrep(j,0,m)
-#define RoundUp(a,b) ((a) % (b) ? (a) / (b) + 1 : (a) / (b))
-#define vi vector<int>
-#define vm vector<mint>
-#define vvi vector<vi>
-#define vdb vector<db>
-#define vb vector<bool>
-#define V vector
+#define overload4(_1,_2,_3,_4,name,...) name
+#define rep1(n) for(int i = 0; i < (n); i++)
+#define rep2(i,n) for(int i = 0; i < (n); i++)
+#define rep3(i,a,b) for(int i = (a); i < (b); i++)
+#define rep4(i,a,b,c) for(int i = (a); i < (b); i += (c))
+#define rep(...) overload4(__VA_ARGS__,rep4,rep3,rep2,rep1)(__VA_ARGS__)
+#define vec(type,name,...) vector<type> name(__VA_ARGS__)
+
+void Main(){
+
+}
+
+signed main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    cout << setprecision(20) << fixed;
+    Main();
+}
