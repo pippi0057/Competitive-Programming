31539b59ee9807158b02c5ed4fe8bad9f6382882
pippi-sniper <67419268+pippi-sniper@users.noreply.github.com>
2021-01-08 11:39:07 +0900

    Update template.cpp

--- library/C++/template.cpp
+++ library/C++/template.cpp
@@ -14,10 +14,11 @@ const int inf = 1e13;
 #define rep(...) overload(__VA_ARGS__,_rep4,_rep3,_rep2,_rep1)(__VA_ARGS__)
 #define _rrep1(n) for(int i = (n) - 1; i >= 0; i--)
 #define _rrep2(i,n) for(int i = (n) - 1; i >= 0; i--)
-#define _rrep3(i,a,b) for(int i = (b) - 1; i >= a; i--)
+#define _rrep3(i,a,b) for(int i = (b) - 1; i >= (a); i--)
 #define rrep(...) overload(__VA_ARGS__,_null,_rrep3,_rrep2,_rrep1)(__VA_ARGS__)
 #define vec(type,name,...) vector<type> name(__VA_ARGS__)
 #define vv(type,name,size,...) vector<vector<type>> name(size,vector<int>(__VA_ARGS__))
+#define ForEach(a,b) for_each(a.begin(),a.end(),b)
 struct Edge { int to, cost; Edge(int to, int cost) : to(to), cost(cost) {} };
 using Graph = vector<vector<Edge>>;
 
@@ -28,7 +29,7 @@ void Main(){
 signed main(){
     cin.tie(0);
     ios::sync_with_stdio(false);
-    cout << setprecision(20) << fixed;
+    cout << setprecision(10) << fixed;
     //int n; cin >> n; rep(n)
     Main();
 }
