65b65fbe04563cdc4b4de34b9956c1fbbe5f0b88
pippi-sniper <67419268+pippi-sniper@users.noreply.github.com>
2021-01-26 08:43:48 +0900

    Update temp_noob.py

--- library/Python/temp_noob.py
+++ library/Python/temp_noob.py
@@ -1,1 +1,17 @@
-# 
+# a(整数)を入力
+a = int(input())
+
+
+# a b(整数を入力)
+a, b = map(int, input().split())
+
+# a
+# b
+# 整数が2行で与えられているときはmapは使えない
+a = int(input())
+b = int(input())
+
+# s(文字列を入力)
+s = input()
+
+# if文と出力はわかるよね＾＾
