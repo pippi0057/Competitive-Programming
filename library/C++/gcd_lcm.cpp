948f218930f080e543581f56244ab3c5dae66731
pippi-sniper <67419268+pippi-sniper@users.noreply.github.com>
2021-02-18 14:46:40 +0900

    配列にも対応(gcd)

--- library/C++/gcd_lcm.cpp
+++ library/C++/gcd_lcm.cpp
@@ -1,2 +1,3 @@
 int gcd(int a, int b) { return b ? gcd(b, a % b) : a; }
 int lcm(int a, int b) { return a * b / gcd(a, b); }
+template <class T> int gcd(vector<T>& v){ int res = 0; for(auto x : v) res = gcd(res, x); return res; }
