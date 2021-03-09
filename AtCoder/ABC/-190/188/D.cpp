9e2e92ca389aeb7a9e8d763a5e39f9a407fd470f
pippi-sniper <poyattosannokeitai@gmail.com>
2021-03-09 13:55:21 +0900

    ABC 188 D

--- AtCoder/ABC/-190/188/D.cpp
+++ AtCoder/ABC/-190/188/D.cpp
@@ -1,8 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define int long long int
+#define ll long long int
 const double pi = 3.14159265358979;
-const int inf = 1e13;
+const ll inf = 1e17;
+const int dx[] = {0, 1, 0, -1, 1, 1, -1, -1};
+const int dy[] = {1, 0, -1, 0, 1, -1, 1, -1};
+#define pii pair<int,int>
+#define pll pair<ll,ll>
 #define endl "\n"
 #define dtor(deg) (((deg)/360)*2*pi)
 #define all(a) a.begin(),a.end()
@@ -15,47 +19,35 @@ const int inf = 1e13;
 #define _rrep1(n) for(int i = (n) - 1; i >= 0; i--)
 #define _rrep2(i,n) for(int i = (n) - 1; i >= 0; i--)
 #define _rrep3(i,a,b) for(int i = (b) - 1; i >= (a); i--)
-#define rrep(...) overload(__VA_ARGS__,_null,_rrep3,_rrep2,_rrep1)(__VA_ARGS__)
+#define _rrep4(i,a,b,c) for(int i = (b) - 1; i >= (a); i -= (c))
+#define rrep(...) overload(__VA_ARGS__,_rrep4,_rrep3,_rrep2,_rrep1)(__VA_ARGS__)
 #define vec(type,name,...) vector<type> name(__VA_ARGS__)
-#define vv(type,name,size,...) vector<vector<type>> name(size,vector<int>(__VA_ARGS__))
+#define vv(type,name,size,...) vector<vector<type>> name(size,vector<type>(__VA_ARGS__))
 #define ForEach(a,b) for_each(a.begin(),a.end(),b)
-struct Edge { int to, cost; Edge(int to, int cost) : to(to), cost(cost) {} };
+struct Edge { int to; ll cost; Edge(int to, ll cost) : to(to), cost(cost) {} };
 using Graph = vector<vector<Edge>>;
+template <class T> bool chmin(T& a, T b){ if(a > b){ a = b; return 1; } return 0; }
+template <class T> bool chmax(T& a, T b){ if(a < b){ a = b; return 1; } return 0; }
 
 void Main(){
-    int n, c, ans = 0;
-    cin >> n >> c;
-    vector<pair<int,int>,int> plan(n);
-    set<int> Split;
+    ll n, cost, ans = 0, now = 0, time = 0;
+    cin >> n >> cost;
+    vector<pll> event;
     rep(n){
-        int a, b, m;
-        cin >> a >> b >> m;
-        Split.insert(a-1);
-        Split.insert(b);
-        plan[i].first.first = a - 1;
-        plan[i].first.second = b;
-        plan[i].second = m;
+        ll a, b, c;
+        cin >> a >> b >> c;
+        a--; b--;
+        event.push_back({a, c});
+        event.push_back({b + 1, -1 * c});
     }
-    vector<int> cnt(Split.size()-1);
-    vector<int> Split_v(Split.size());
-    int temp = 0;
-    map<int,int> access_1, access_2;
-    for(auto i : Split){
-        if(temp != Split.size() - 1) access_1[i] = temp;
-        if(temp != 0) access_2[i] = temp - 1;
-        Split_v[temp] = i;
-        temp++;
-    }
-    rep(Split_v.size()-1) cnt[i] = Split_v[i+1] - Split_v[i];
-    vector<int> imos(Split.size()-1);
-    rep(n){
-        imos[access_1[plan[i].first.first]] += plan[i].second;
-        imos[access_2[plan[i].first.second]] -= plan[i].second;
-    }
-    int now = 0;
-    rep(imos.size()){
-        now += imos[i];
-        ans += min(c, now) * cnt[i];
+    sort(all(event));
+    for(auto e : event){
+        ll a = e.first, b = e.second;
+        if(a != time){
+            ans += min(cost, now) * (a - time);
+            time = a;
+        }
+        now += b;
     }
     cout << ans << endl;
 }
@@ -64,6 +56,5 @@ signed main(){
     cin.tie(0);
     ios::sync_with_stdio(false);
     cout << setprecision(10) << fixed;
-    //int n; cin >> n; rep(n)
     Main();
 }
