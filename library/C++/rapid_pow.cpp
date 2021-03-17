b296a104c0487da1fe9779b6b16ef2446af9b10c
pippi-sniper <67419268+pippi-sniper@users.noreply.github.com>
2021-03-17 14:15:45 +0900

    Update rapid_pow.cpp

--- library/C++/rapid_pow.cpp
+++ library/C++/rapid_pow.cpp
@@ -1,10 +1,5 @@
-/**
- *   高速べき乗 O(log n)
- *   rapid_pow<mod>(n, m)
-**/
-
-int mow(int x, int n, int mod) {
-    int ret = 1;
+ll mow(ll x, ll n, ll mod) {
+    ll ret = 1;
     while (n > 0) {
         if (n & 1) ret = ret * x % mod;
         x = x * x % mod;
