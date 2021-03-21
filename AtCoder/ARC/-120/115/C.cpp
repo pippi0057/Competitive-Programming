c947a5fb8e039bee76fc39f9970257afe16f77dc
pippi-sniper <poyattosannokeitai@gmail.com>
2021-03-22 00:13:46 +0900

    ARC 115

---
+++ AtCoder/ARC/-120/115/C.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+const double pi = 3.14159265358979;
+const ll inf = 1e17;
+const int dx[] = {0, 1, 0, -1, 1, 1, -1, -1};
+const int dy[] = {1, 0, -1, 0, 1, -1, 1, -1};
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define endl "\n"
+#define dtor(deg) (((deg)/360)*2*pi)
+#define all(a) a.begin(),a.end()
+#define overload(_1,_2,_3,_4,name,...) name
+#define _rep1(n) for(int i = 0; i < (n); i++)
+#define _rep2(i,n) for(int i = 0; i < (n); i++)
+#define _rep3(i,a,b) for(int i = (a); i < (b); i++)
+#define _rep4(i,a,b,c) for(int i = (a); i < (b); i += (c))
+#define rep(...) overload(__VA_ARGS__,_rep4,_rep3,_rep2,_rep1)(__VA_ARGS__)
+#define _rrep1(n) for(int i = (n) - 1; i >= 0; i--)
+#define _rrep2(i,n) for(int i = (n) - 1; i >= 0; i--)
+#define _rrep3(i,a,b) for(int i = (b) - 1; i >= (a); i--)
+#define _rrep4(i,a,b,c) for(int i = (b) - 1; i >= (a); i -= (c))
+#define rrep(...) overload(__VA_ARGS__,_rrep4,_rrep3,_rrep2,_rrep1)(__VA_ARGS__)
+#define vec(type,name,...) vector<type> name(__VA_ARGS__)
+#define vv(type,name,size,...) vector<vector<type>> name(size,vector<type>(__VA_ARGS__))
+#define ForEach(a,b) for_each(a.begin(),a.end(),b)
+template <class T> bool chmin(T& a, T b){ if(a > b){ a = b; return 1; } return 0; }
+template <class T> bool chmax(T& a, T b){ if(a < b){ a = b; return 1; } return 0; }
+
+vector<ll> divisor(ll n){
+    vector<ll> res;
+    for(ll i = 1; i * i <= n; i++){
+        if (n % i == 0) {
+            res.push_back(i);
+            if (i * i != n) res.push_back(n / i);
+        }
+    }
+    sort(all(res));
+    return res;
+}
+
+ll divisor_count(ll n){
+    ll res = 1;
+    for (ll i = 2; i * i <= n; i++) {
+        ll cnt = 0;
+        while (n % i == 0) {
+            cnt++;
+            n /= i;
+        }
+        res *= (cnt + 1);
+        if(n == 1) break;
+    }
+    if(n != 1) res *= 2;
+    return res;
+}
+
+bool is_prime(int n){
+    if (n < 2) return 0;
+    else if (n == 2) return 1;
+    else if (n % 2 == 0) return 0;
+    for(int i = 3; i * i <= n; i += 2) if(n % i == 0) return 0;
+    return 1;
+}
+
+void Main(){
+    ll n, div = 0;
+    cin >> n;
+    vector<ll> ans(n, inf);
+    ans[0] = 1;
+    rep(i,1,n+1) chmax(div, divisor_count(i));
+    for(ll i = n; i >= 2; i--){
+        if(ans[i-1] != inf) continue;
+        vector<ll> temp = divisor(i);
+        ll copy = div;
+        rrep(j,temp.size()){
+            if(ans[temp[j]-1] != inf) break;
+            ans[temp[j]-1] = copy;
+            copy--;
+        }
+    }
+    rep(n) cout << ans[i] << " ";
+}
+
+signed main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    cout << setprecision(10) << fixed;
+    Main();
+}
