d01dbf0b3dd6dd92b553b6e5bcdcf1dca9062f07
pippi-sniper <poyattosannokeitai@gmail.com>
2021-03-21 19:44:01 +0900

    ABC 196

---
+++ AtCoder/ABC/191-/196/D.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+const double pi = 3.14159265358979;
+const ll inf = 1e17;
+const int dx[] = {0, 1, 0, -1, 1, 1, -1, -1};
+const int dy[] = {1, 0, -1, 0, 1, -1, 1, -1};
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define endl "\n"
+#define dtor(deg) (((deg)/360)*2*pi)
+#define all(a) a.begin(),a.end()
+#define overload(_1,_2,_3,_4,name,...) name
+#define _rep1(n) for(int i = 0; i < (n); i++)
+#define _rep2(i,n) for(int i = 0; i < (n); i++)
+#define _rep3(i,a,b) for(int i = (a); i < (b); i++)
+#define _rep4(i,a,b,c) for(int i = (a); i < (b); i += (c))
+#define rep(...) overload(__VA_ARGS__,_rep4,_rep3,_rep2,_rep1)(__VA_ARGS__)
+#define _rrep1(n) for(int i = (n) - 1; i >= 0; i--)
+#define _rrep2(i,n) for(int i = (n) - 1; i >= 0; i--)
+#define _rrep3(i,a,b) for(int i = (b) - 1; i >= (a); i--)
+#define _rrep4(i,a,b,c) for(int i = (b) - 1; i >= (a); i -= (c))
+#define rrep(...) overload(__VA_ARGS__,_rrep4,_rrep3,_rrep2,_rrep1)(__VA_ARGS__)
+#define vec(type,name,...) vector<type> name(__VA_ARGS__)
+#define vv(type,name,size,...) vector<vector<type>> name(size,vector<type>(__VA_ARGS__))
+#define ForEach(a,b) for_each(a.begin(),a.end(),b)
+template <class T> bool chmin(T& a, T b){ if(a > b){ a = b; return 1; } return 0; }
+template <class T> bool chmax(T& a, T b){ if(a < b){ a = b; return 1; } return 0; }
+
+int solve_one(int w, int a, int b){
+    if(w <= 0 || a < 0) return 0;
+    if(w == 1) return 1;
+    if(w == 2) return 1;
+    if(w == 3){
+        if(a == 1) return 2;
+        if(a == 0) return 1;
+    }
+    return solve_one(w - 2, a - 1, b) + solve_one(w - 1, a, b - 1);
+}
+
+int solve_two(int w, int a, int b){
+    if(w <= 0 || a < 0) return 0;
+    if(w == 1) return 1;
+    if(w == 2){
+        if(a == 2) return 1;
+        if(a == 1) return 4;
+        if(a == 0) return 1;
+    }
+    return 4 * solve_two(w - 2, a - 1, b - 2) + solve_two(w - 1, a - 1, b) + solve_two(w - 1, a, b - 2);
+}
+
+int solve_four(int w, int a, int b){
+    int res = 0;
+    rep(a+1){
+        res += solve_two(w, i, b) + solve_two(w, a - i, b);
+    }
+    return res;
+}
+
+void Main(){
+    int h, w, a, b;
+    cin >> h >> w >> a >> b;
+    if(h > w) swap(h, w);
+    if(h == 1) cout << solve_one(w, a, b) << endl;
+    else if(h == 2) cout << solve_two(w, a, b) << endl;
+    else if(h == 4) cout << solve_four(w, a, b) << endl;
+}
+
+signed main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    cout << setprecision(10) << fixed;
+    Main();
+}
