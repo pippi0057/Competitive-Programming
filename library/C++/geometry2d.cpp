346f1984d6cc79da3edf826d99ebf65e90688476
pippi-sniper <poyattosannokeitai@gmail.com>
2021-04-01 01:26:34 +0900

    直線・半直線・線分(未完成)

--- library/C++/geometry2d.cpp
+++ library/C++/geometry2d.cpp
@@ -1,4 +1,4 @@
-namespace geometry2d{
+namespace Geometry2D{
     const double EPS = 1e-10;
     const double PI = 3.14159265358979;
     double dtor(double d){ return d * PI / 180.0; }
@@ -35,6 +35,7 @@ namespace geometry2d{
         Point rotation(double arg) const{ double cs = cos(arg), sn = sin(arg); return Point(x * cs - y * sn, x * sn + y * cs); }
         double angle() const{ return atan2(y, x); }
     };
+    const Point error_val = { 123456789.123, 123456789.123 };
     int iSP(Point a, Point b, Point c){
         int flg = sgn((b - a).cross(c - a));
         if(flg == 1 || flg == -1) return flg;
@@ -43,4 +44,37 @@ namespace geometry2d{
         return 0;
     }
     int angleType(Point a, Point b, Point c){ double v = (a - b).dot(c - b); return sgn(v) > 0 ? 0 : (sgn(v) == 0 ? 1 : 2); }
+    namespace Line2D{
+        struct Line{
+            Point from, to;
+            Line(){ from = Point(), to = Point(); }
+            Line(const Point& f, const Point& t){ from = f, to = t; }
+            Line(const double a, const double b, const double c) {
+                if(sgn(a) == 0 && sgn(b) == 0) assert(-1);
+                if(sgn(b) == 0){ from = Point(-c / a, 0.0); to = Point(-c / a, 1.0); }
+                else{ from = Point(0, -c / b); to = Point(1.0, -(a + c) / b); }
+            }
+            Point toVec() const{ return to - from; }
+            Point toVecRev() const{ return from - to; }
+        };
+        typedef Line Ray;
+        typedef Line Segment;
+        pair<Point, bool> lineInterSection(const Line& l1, const Line& l2){
+            if(sgn(l1.toVec().cross(l2.toVec())) == 0) return{ error_val, false };
+            return{ l1.from + l1.toVec() * abs((l2.to - l1.from).cross(l2.toVec()) / l1.toVec().cross(l2.toVec())), true };
+        }
+        double distBetweenPointAndLine(const Point& p, const Line& l){ return abs(l.toVec().cross(p - l.from) / l.toVec().length()); }
+        double distBetweenPointAndRay(const Point& p, const Ray& r){
+            if(sgn((p - r.from).dot(r.toVec())) < 0) return r.from.dist(p);
+            return abs(r.toVec().cross(p - r.from) / r.toVec().length());
+        }
+        double distBetweenPointAndSegment(const Point& p, const Segment& s){
+            if(sgn(s.toVec().dot(p - s.from)) < 0 || sgn(s.toVecRev().dot(p - s.to)) < 0)
+                return min(p.dist(s.from), p.dist(s.to));
+            return distBetweenPointAndLine(p, s);
+        }
+        double distBetweenSegmentAndSegment(const Segment& s1, const Segment& s2){
+
+        }
+    }
 }
