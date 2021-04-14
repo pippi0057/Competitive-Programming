91f2e561099561269a083a98b822972d93c6c726
pippi-sniper <poyattosannokeitai@gmail.com>
2021-04-15 00:16:18 +0900

    ABC 165 B

--- AtCoder/ABC/-180/165/B.cpp
+++ AtCoder/ABC/-180/165/B.cpp
@@ -1,36 +1,46 @@
 #include <bits/stdc++.h>
+#include <atcoder/all>
 using namespace std;
-#define int long long int
-const double pi = 3.14159265358979;
-const int inf = 1e13;
-#define setup cin.tie(0); ios::sync_with_stdio(false); cout << setprecision(20) << fixed;
-#define db double
-#define P pair<int,int>
-#define F first
-#define S second
+using namespace atcoder;
+#define ll int64_t
+const long double EPS = 1e-10;
+const ll inf = 1e17;
+const int dx[] = {0, 1, 0, -1, 1, 1, -1, -1};
+const int dy[] = {1, 0, -1, 0, 1, -1, 1, -1};
+#define pii pair<int,int>
+#define pll pair<ll,ll>
 #define endl "\n"
-#define dtor(deg) (((deg)/360)*2*pi)
-#define rtod(rad) (((rad)/2/pi)*360)
 #define all(a) a.begin(),a.end()
-#define Srep(n) for(int i = 0; i < n; i++)
-#define Lrep(i,a,n) for(int i = a; i < n; i++)
-#define Brep1st(n) for(int bit = 0; bit < (1 << n); bit++)
-#define Brep2nd(n) Srep(n) if(bit >> i & 1)
-#define rep2d(n,m) Srep(n) Lrep(j,0,m)
-#define RoundUp(a,b) ((a) % (b) ? (a) / (b) + 1 : (a) / (b))
-#define vi vector<int>
-#define vvi vector<vi>
-#define vdb vector<db>
-#define vb vector<bool>
-#define V vector
+#define overload(_1,_2,_3,_4,name,...) name
+#define _rep1(n) for(int i = 0; i < (n); i++)
+#define _rep2(i,n) for(int i = 0; i < (n); i++)
+#define _rep3(i,a,b) for(int i = (a); i < (b); i++)
+#define _rep4(i,a,b,c) for(int i = (a); i < (b); i += (c))
+#define rep(...) overload(__VA_ARGS__,_rep4,_rep3,_rep2,_rep1)(__VA_ARGS__)
+#define _rrep1(n) for(int i = (n) - 1; i >= 0; i--)
+#define _rrep2(i,n) for(int i = (n) - 1; i >= 0; i--)
+#define _rrep3(i,a,b) for(int i = (b) - 1; i >= (a); i--)
+#define _rrep4(i,a,b,c) for(int i = (b) - 1; i >= (a); i -= (c))
+#define rrep(...) overload(__VA_ARGS__,_rrep4,_rrep3,_rrep2,_rrep1)(__VA_ARGS__)
+#define vec(type,name,...) vector<type> name(__VA_ARGS__)
+#define vv(type,name,size,...) vector<vector<type>> name(size,vector<type>(__VA_ARGS__))
+#define ForEach(a,b) for_each(a.begin(),a.end(),b)
+#define NextAfter(x) x = nextafter(x, INFINITY)
+template <class T> bool chmin(T& a, T b){ if(a > b){ a = b; return 1; } return 0; }
+template <class T> bool chmax(T& a, T b){ if(a < b){ a = b; return 1; } return 0; }
+
+void Main(){
+    ll x, sum = 100;
+    cin >> x;
+    for(int i = 1; ; i++){
+        sum += sum / 100;
+        if(sum >= x){ cout << i << endl; return; }
+    }
+}
 
 signed main(){
-    setup;
-    int x, ans = 0, m = 100;
-    cin >> x;
-    while(m < x){
-        m += m / 100;
-        ans++;
-    }
-    cout << ans << endl;
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    cout << setprecision(10) << fixed;
+    Main();
 }
