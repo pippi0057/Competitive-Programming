628d07e1700a0f9224a329d5f4d2e4cee46cf54f
pippi-sniper <poyattosannokeitai@gmail.com>
2021-04-20 21:43:46 +0900

    拡張ユークリッド

--- library/C++/gcd_lcm.cpp
+++ library/C++/gcd_lcm.cpp
@@ -1,4 +1,10 @@
-int gcd(int a, int b) { return b ? gcd(b, a % b) : a; }
-int lcm(int a, int b) { return a * b / gcd(a, b); }
-template <class T> int gcd(vector<T>& v){ int res = 0; for(auto x : v) res = gcd(res, x); return res; }
-template <class T> int lcm(vector<T>& v){ int res = 1; for(auto x : v) res = lcm(res, x); return res; }
+ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
+ll lcm(ll a, ll b) { return a * b / gcd(a, b); }
+template <class T> ll gcd(vector<T>& v){ ll res = 0; for(auto x : v) res = gcd(res, x); return res; }
+template <class T> ll lcm(vector<T>& v){ ll res = 1; for(auto x : v) res = lcm(res, x); return res; }
+ll gcdExt(ll a, ll b, ll& x, ll& y){
+    if(!b){ x = 1; y = 0; return a; }
+    ll d = gcdExt(b, a % b, y, x);
+    y -= a / b * x;
+    return d;
+}
