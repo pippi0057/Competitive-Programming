2d5f07eb96d0efc0bfeffb2fa968bbab5a8be333
pippi-sniper <67419268+pippi-sniper@users.noreply.github.com>
2021-04-26 10:31:02 +0900

    Update mow.cpp

--- library/C++/mow.cpp
+++ library/C++/mow.cpp
@@ -1,9 +1,2 @@
-ll mow(ll x, ll n, ll mod) {
-    ll ret = 1;
-    while (n > 0) {
-        if (n & 1) ret = ret * x % mod;
-        x = x * x % mod;
-        n >>= 1;
-    }
-    return ret;
-}
+ll mow(ll x, ll n){ ll ret = 1; while(n > 0){ if(n & 1) ret = ret * x; x = x * x; n >>= 1; } return ret; }
+ll mow(ll x, ll n, ll mod){ ll ret = 1; while(n > 0){ if(n & 1) ret = ret * x % mod; x = x * x % mod; n >>= 1; } return ret; }
