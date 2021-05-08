76d3465fbd218b15a8d110100c8e5a6f192765ab
pippi-sniper <poyattosannokeitai@gmail.com>
2021-05-09 01:35:26 +0900

    ABC 200

---
+++ AtCoder/ABC/191-/200/D.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+#define ll int64_t
+const ll inf = 1e17;
+const int dx[] = {0, 1, 0, -1, 1, 1, -1, -1};
+const int dy[] = {1, 0, -1, 0, 1, -1, 1, -1};
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define endl "\n"
+#define all(a) a.begin(),a.end()
+#define overload(_1,_2,_3,_4,name,...) name
+#define _rep1(n) for(int i = 0; i < (n); i++)
+#define _rep2(i,n) for(int i = 0; i < (n); i++)
+#define _rep3(i,a,b) for(int i = (a); i < (b); i++)
+#define _rep4(i,a,b,c) for(int i = (a); i < (b); i += (c))
+#define rep(...) overload(__VA_ARGS__,_rep4,_rep3,_rep2,_rep1)(__VA_ARGS__)
+#define _rrep1(n) for(int i = (n) - 1; i >= 0; i--)
+#define _rrep2(i,n) for(int i = (n) - 1; i >= 0; i--)
+#define _rrep3(i,a,b) for(int i = (b) - 1; i >= (a); i--)
+#define _rrep4(i,a,b,c) for(int i = (b) - 1; i >= (a); i -= (c))
+#define rrep(...) overload(__VA_ARGS__,_rrep4,_rrep3,_rrep2,_rrep1)(__VA_ARGS__)
+#define vec(type,name,...) vector<type> name(__VA_ARGS__)
+#define vv(type,name,size,...) vector<vector<type>> name(size,vector<type>(__VA_ARGS__))
+#define ForEach(a,b) for_each(a.begin(),a.end(),b)
+#define NextAfter(x) x = nextafter(x, INFINITY)
+template <class T> bool chmin(T& a, T b){ if(a > b){ a = b; return 1; } return 0; }
+template <class T> bool chmax(T& a, T b){ if(a < b){ a = b; return 1; } return 0; }
+
+void Main(){
+    int n;
+    cin >> n;
+    vector a(n, 0);
+    for(auto& x : a) cin >> x;
+    vector<vector<vector<vector<int>>>> dp(n, vector<vector<vector<int>>>(200));
+    dp[0][0].push_back({});
+    dp[0][a[0]%200].push_back({0});
+    for(int i = 1; i < n; i++){
+        for(int j = 0; j < 200; j++){
+            for(int k = 0; k < min(2, int(dp[i-1][j].size())); k++){
+                dp[i][(j+a[i])%200].push_back(dp[i-1][j][k]);
+                dp[i][(j+a[i])%200][dp[i][(j+a[i])%200].size()-1].push_back(i);
+            }
+            for(int k = 0; k < min(2, int(dp[i-1][j].size())); k++){
+                dp[i][j].push_back(dp[i-1][j][k]);
+            }
+        }
+    }
+    rep(200){
+        if(i){ if(dp[n-1][i].size() > 1){
+            cout << "Yes" << endl << dp[n-1][i][0].size();
+            for(auto x : dp[n-1][i][0]) cout << " " << x + 1;
+            cout << endl << dp[n-1][i][1].size();
+            for(auto& x : dp[n-1][i][1]) cout << " " << x + 1;
+            cout << endl;
+            return;
+        }}else{ if(dp[n-1][i].size() > 2){
+            int cnt = 0;
+            cout << "Yes" << endl;
+            if(dp[n-1][i][0].size()){
+                cout << dp[n-1][i][0].size();
+                for(auto x : dp[n-1][i][0]) cout << " " << x + 1;
+                cout << endl;
+                cnt++;
+            }
+            if(dp[n-1][i][1].size()){
+                cout << dp[n-1][i][1].size();
+                for(auto x : dp[n-1][i][1]) cout << " " << x + 1;
+                cout << endl;
+                cnt++;
+            }
+            if(dp[n-1][i][2].size() && cnt < 2){
+                cout << dp[n-1][i][2].size();
+                for(auto x : dp[n-1][i][2]) cout << " " << x + 1;
+                cout << endl;
+            }
+            return;
+        }}
+    }
+    cout << "No" << endl;
+}
+
+signed main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    cout << setprecision(10) << fixed;
+    Main();
+}
