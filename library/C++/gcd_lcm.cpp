3e2c283698341171376cae779bc7dcf3e41981ff
pippi-sniper <67419268+pippi-sniper@users.noreply.github.com>
2021-06-12 09:25:37 +0900

    Add binary_gcd

--- library/C++/gcd_lcm.cpp
+++ library/C++/gcd_lcm.cpp
@@ -1,4 +1,10 @@
 ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
+ll binary_gcd(ll a, ll b){
+    if(!a) return b; if(!b) return a;
+    if(~a & 1 && ~b & 1) return binary_gcd(a / 2, b / 2) * 2;
+    if(~a & 1) return binary_gcd(a / 2, b); if(~b & 1) return binary_gcd(a, b / 2);
+    if(a < b) return binary_gcd(a, (b - a) / 2); return binary_gcd(b, (a - b) / 2);
+}
 ll lcm(ll a, ll b) { return a * b / gcd(a, b); }
 template <class T> ll gcd(vector<T>& v){ ll res = 0; for(auto x : v) res = gcd(res, x); return res; }
 template <class T> ll lcm(vector<T>& v){ ll res = 1; for(auto x : v) res = lcm(res, x); return res; }
