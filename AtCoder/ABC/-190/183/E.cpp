fa349891c5a8156423d4b7f7eacaa6acf6bdf0dc
pippi-sniper <poyattosannokeitai@gmail.com>
2021-07-23 23:04:37 +0900

    ABC183E

--- AtCoder/ABC/-190/183/E.cpp
+++ AtCoder/ABC/-190/183/E.cpp
@@ -1,55 +1,60 @@
 #include <bits/stdc++.h>
+#include <atcoder/all>
 using namespace std;
-int scan() { return getchar(); }
-void scan(int &a) { cin >> a; }
-void scan(long long &a) { cin >> a; }
-void scan(char &a) { cin >> a; }
-void scan(double &a) { cin >> a; }
-void scan(string &a) { cin >> a; }
-template <class T, class S> void scan(pair<T, S> &p) { scan(p.first), scan(p.second); }
-template <class T> void scan(vector<T> &);
-template <class T> void scan(vector<T> &a) {
-    for(auto &i : a) scan(i);
-}
-template <class T> void scan(T &a) { cin >> a; }
-void IN() {}
-template <class Head, class... Tail> void IN(Head &head, Tail &... tail) {
-    scan(head);
-    IN(tail...);
-}
-#define all(a) a.begin(),a.end() 
-#define Srep(n) for(int64_t i = 0; i < int64_t(n); i++)
-#define Lrep(i,a,n) for(int64_t i = int64_t(a); i < int64_t(n); i++)
-#define Brep1st(n) for(int bit = 0; bit < (1 << n); bit++)
-#define Brep2nd(n) Srep(n) if(bit >> i & 1)
-#define rep2d(n,m) Srep(n) Lrep(j,0,m)
-#define vec(type,name) vector<type> name
-#define vecc(type,name,...) vector<type> name(__VA_ARGS__)
-#define vvec(type,name,size,...) vector<vector<type>> name(size, vector<type>(__VA_ARGS__))
-#define scan(...) IN(__VA_ARGS__)
-#define intscan(...)\
-    int __VA_ARGS__;\
-    IN(__VA_ARGS__)
-#define i64scan(...)\
-    int64_t __VA_ARGS__;\
-    IN(__VA_ARGS__)
-#define charscan(...)\
-    char __VA_ARGS__;\
-    IN(__VA_ARGS__)
-#define strscan(...)\
-    string __VA_ARGS__;\
-    IN(__VA_ARGS__)
-#define vecscan(type,name,size)\
-    vector<type> name(size);\
-    IN(name)
-#define vvecscan(type,name,size1,size2)\
-    vector<vector<type>> name(size1, vector<type>(size2));\
-    IN(name)
+using namespace atcoder;
+#define ll int64_t
+#define u32 uint32_t
+#define u64 uint64_t
+constexpr ll inf = 1e17;
+constexpr int dx[] = {0, 1, 0, -1, 1, 1, -1, -1};
+constexpr int dy[] = {1, 0, -1, 0, 1, -1, 1, -1};
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define endl "\n"
+#define all(a) a.begin(),a.end()
+#define overload(_1,_2,_3,_4,name,...) name
+#define _rep1(n) for(int i = 0; i < (n); i++)
+#define _rep2(i,n) for(int i = 0; i < (n); i++)
+#define _rep3(i,a,b) for(int i = (a); i < (b); i++)
+#define _rep4(i,a,b,c) for(int i = (a); i < (b); i += (c))
+#define rep(...) overload(__VA_ARGS__,_rep4,_rep3,_rep2,_rep1)(__VA_ARGS__)
+#define _rrep1(n) for(int i = (n) - 1; i >= 0; i--)
+#define _rrep2(i,n) for(int i = (n) - 1; i >= 0; i--)
+#define _rrep3(i,a,b) for(int i = (b) - 1; i >= (a); i--)
+#define _rrep4(i,a,b,c) for(int i = (b) - 1; i >= (a); i -= (c))
+#define rrep(...) overload(__VA_ARGS__,_rrep4,_rrep3,_rrep2,_rrep1)(__VA_ARGS__)
+#define vv(type,name,size,...) vector<vector<type>> name(size,vector<type>(__VA_ARGS__))
+#define ForEach(a,b) for_each(a.begin(),a.end(),b)
+#define NextAfter(x) x = nextafter(x, INFINITY)
+template <class T> inline bool chmin(T& a, T b){ if(a > b){ a = b; return 1; } return 0; }
+template <class T> inline bool chmax(T& a, T b){ if(a < b){ a = b; return 1; } return 0; }
+struct Edge { int to; ll cost; Edge(int to, ll cost) : to(to), cost(cost) {} };
+using Graph = vector<vector<Edge>>;
 
-int main(){
-    intscan(h,w);
-    vvecscan(char,maze,h,w);
-    vvec(int,ans,h,w,0);
-    int sx = 0, sy = 0, gx = h - 1, gy = w - 1;
-    queue<pair<int,int>> que;
+using mint = modint1000000007;
+
+void Main(){
+    int h, w;
+    cin >> h >> w;
+    vector s(h, vector(w, false));
+    rep(h) rep(j,w){
+        char input;
+        cin >> input;
+        s[i][j] = input == '#';
+    }
+    vector<vector<mint>> dp(h, vector<mint>(w)), a(h, vector<mint>(w)), b(h, vector<mint>(w)), c(h, vector<mint>(w));
+    dp[0][0] = 1;
+    rep(h) rep(j,w) if(!s[i][j]){
+        if(0 < i) a[i][j] += a[i-1][j] + dp[i-1][j], dp[i][j] += a[i][j];
+        if(0 < j) b[i][j] += b[i][j-1] + dp[i][j-1], dp[i][j] += b[i][j];
+        if(0 < i && 0 < j) c[i][j] += c[i-1][j-1] + dp[i-1][j-1], dp[i][j] += c[i][j];
+    }
+    cout << dp[h-1][w-1].val() << endl;
+}
+
+signed main(){
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+    cout << setprecision(10) << fixed;
+    Main();
 }
