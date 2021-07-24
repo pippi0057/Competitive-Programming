c1ca061be84a23be7135c81aaf83953dcc98c0a0
pippi-sniper <poyattosannokeitai@gmail.com>
2021-07-24 20:04:21 +0900

    ABC210 C

--- AtCoder/ABC/201-/210/E.cpp
+++ AtCoder/ABC/201-/210/E.cpp
@@ -31,10 +31,28 @@ template <class T> inline bool chmax(T& a, T b){ if(a < b){ a = b; return 1; } r
 struct Edge { int to; ll cost; Edge(int to, ll cost) : to(to), cost(cost) {} };
 using Graph = vector<vector<Edge>>;
 
+add?struct UnionFind{
+    vector<ll> data;
+    UnionFind(ll n): data(n, -1){}
+    bool unite(ll a, ll b){
+        a = root(a); b = root(b);
+        if(a == b) return 0;
+        if(data[a] > data[b]) swap(a, b);
+        data[a] += data[b];
+        data[b] = a;
+        return 1;
+    }
+    bool find(ll a, ll b){ return root(a) == root(b); }
+    ll root(ll a){ return data[a] < 0 ? a : data[a] = root(data[a]); }
+    ll size(ll a){ return -data[root(a)]; }
+    ll operator[](ll a){ return root(a); }
+};
+
+bool operator<()
+
 void Main(){
     int n, m;
     cin >> n >> m;
-    Graph g(n);
     vector<pii> data(m);
     for(auto& [a, c] : data) cin >> a >> c;
     
