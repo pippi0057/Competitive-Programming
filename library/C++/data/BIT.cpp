bf6d31680abf1fb2f28bde3e4a9fc839d5e41bd1
pippi-sniper <poyattosannokeitai@gmail.com>
2021-10-10 17:41:54 +0900

    updated BIT

--- library/C++/data/BIT.cpp
+++ library/C++/data/BIT.cpp
@@ -1,21 +1,11 @@
-/**
- *  binary indexed tree(BIT) [0-indexed]
- *  add(index, x) O(log n)
- *  sum(index) O(log n)
-**/
-
 template <class T> struct BIT{
-    int n;
-    vector<T> bit;
-    BIT(int n) : n(n+1), bit(n, 0) {}
-    void add(int ind, T x){
-        for(int i = ind; i < n; i += ((i+1) & (-i-1)))
-            bit[i] += x;
-    }
-    T sum(int ind){
-        T res = 0;
-        for(int i = ind-1; i >= 0; i -= ((i+1) & (-i-1)))
-            res += bit[i];
+    int n; vector<T> bit;
+    BIT(int n){ this->n = n; bit.resize(n, 0); }
+    BIT(vector<T> v) : BIT(v.size()) { rep(v.size()) add(i, v[i]); }
+    inline void add(int i, T x){ for(; i < n; i = i | (i + 1)) bit[i] += x; }
+    inline T sum(int i){
+        T res = 0; i--;
+        for(; i >= 0; i = (i & (i + 1)) - 1) res += bit[i];
         return res;
     }
 };
