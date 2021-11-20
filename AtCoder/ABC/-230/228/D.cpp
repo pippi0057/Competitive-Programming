2ff6f5875fba15ff007ab46984c14baa4ac69ac2
pippi-sniper <poyattosannokeitai@gmail.com>
2021-11-21 01:52:35 +0900

    ABC228

---
+++ AtCoder/ABC/-230/228/D.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+#define ll int64_t
+#define u32 uint32_t
+#define u64 uint64_t
+constexpr ll inf = 1e17;
+constexpr int dx[] = {0, 1, 0, -1, 1, 1, -1, -1};
+constexpr int dy[] = {1, 0, -1, 0, 1, -1, 1, -1};
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define endl "\n"
+#define Auto const auto
+#define all(a) a.begin(),a.end()
+#define overload(_1,_2,_3,_4,name,...) name
+#define _rep1(n) for(int i = 0; i < (n); i++)
+#define _rep2(i,n) for(int i = 0; i < (n); i++)
+#define _rep3(i,a,b) for(int i = (a); i < (b); i++)
+#define _rep4(i,a,b,c) for(int i = (a); i < (b); i += (c))
+#define rep(...) overload(__VA_ARGS__,_rep4,_rep3,_rep2,_rep1)(__VA_ARGS__)
+#define _rrep1(n) for(int i = (n) - 1; i >= 0; i--)
+#define _rrep2(i,n) for(int i = (n) - 1; i >= 0; i--)
+#define _rrep3(i,a,b) for(int i = (b) - 1; i >= (a); i--)
+#define _rrep4(i,a,b,c) for(int i = (b) - 1; i >= (a); i -= (c))
+#define rrep(...) overload(__VA_ARGS__,_rrep4,_rrep3,_rrep2,_rrep1)(__VA_ARGS__)
+#define vv(type,name,size,...) vector<vector<type>> name(size,vector<type>(__VA_ARGS__))
+template <class T> inline bool chmin(T& a, T b){ if(a > b){ a = b; return 1; } return 0; }
+template <class T> inline bool chmax(T& a, T b){ if(a < b){ a = b; return 1; } return 0; }
+struct Edge { int to; ll cost; Edge(int to, ll cost) : to(to), cost(cost) {} };
+using Graph = vector<vector<Edge>>;
+template<class T> istream& operator>>(istream& is, vector<T>& a){ for(auto& x : a) is >> x; return is; }
+template<class T> void operator+=(vector<T>& a, T b){ a.push_back(b); return; }
+template<class T> istream& operator>>(istream& is, set<T>& a){ T input; is >> input; a.insert(input); return is; }
+struct sorted_impl{
+    template<class T> friend vector<T> operator|(vector<T> a, sorted_impl){ sort(all(a)); return a; }
+    template<class T> friend void operator|=(vector<T>& a, sorted_impl){ sort(all(a)); }
+} sorted;
+struct reversed_impl{
+    template<class T> friend vector<T> operator|(vector<T> a, reversed_impl){ reverse(all(a)); return a; }
+    template<class T> friend void operator|=(vector<T>& a, reversed_impl){ reverse(all(a)); }
+} reversed;
+
+void Main(){
+    int q; ll n = 1 << 20, cnt = 0;
+    vector<pair<ll, int>> a(n, {-1, 0}), b;
+    rep(n) a[i].second = i;
+    cin >> q;
+    while(q--){
+        int t; ll x, m;
+        cin >> t >> x;
+        m = x % n;
+        if(t == 1){
+            bool ok = 0;
+            rep(20) if(cnt == (1 << i)) ok = 1;
+            if(ok){
+                b.resize(0);
+                rep(n) if(a[i].first == -1) b += a[i];
+            }
+            rep(n) if(b[i].second == m){ m = i; break; }
+            while(1){
+                if(b[m].first == -1){ m = b[i].second; break; }
+                m++; m %= n;
+            }
+            a[m].first = x;
+            cnt++;
+        }else{
+            cout << a[m] << endl;
+        }
+    }
+}
+
+signed main(){
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+    cout << setprecision(10) << fixed;
+    Main();
+}
