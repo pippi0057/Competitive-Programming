611d844a84541dd05635bf6a79ac27bbd0146db5
pippi-sniper <poyattosannokeitai@gmail.com>
2022-01-15 22:39:43 +0900

    ABC180E

--- AtCoder/ABC/-180/180/E.cpp
+++ AtCoder/ABC/-180/180/E.cpp
@@ -24,28 +24,37 @@ constexpr int dy[] = {1, 0, -1, 0, 1, -1, 1, -1};
 #define _rrep3(i,a,b) for(int i = (b) - 1; i >= (a); i--)
 #define _rrep4(i,a,b,c) for(int i = (b) - 1; i >= (a); i -= (c))
 #define rrep(...) overload(__VA_ARGS__,_rrep4,_rrep3,_rrep2,_rrep1)(__VA_ARGS__)
-#define vv(type,name,size,...) vector<vector<type>> name(size,vector<type>(__VA_ARGS__))
+#define F function
 template <class T> inline bool chmin(T& a, T b){ if(a > b){ a = b; return 1; } return 0; }
 template <class T> inline bool chmax(T& a, T b){ if(a < b){ a = b; return 1; } return 0; }
 struct Edge { int to; ll cost; Edge(int to, ll cost) : to(to), cost(cost) {} };
 using Graph = vector<vector<Edge>>;
+template<class T> istream& operator>>(istream& is, vector<T>& a){ for(auto& x : a) is >> x; return is; }
+template<class T> void operator+=(vector<T>& a, T b){ a.push_back(b); return; }
+template<class T> istream& operator>>(istream& is, set<T>& a){ T input; is >> input; a.insert(input); return is; }
+template<class T> void operator+=(set<T>& a, T b){ a.insert(b); return; }
+struct sorted_impl{
+    template<class T> friend vector<T> operator|(vector<T> a, sorted_impl){ sort(all(a)); return a; }
+    template<class T> friend void operator|=(vector<T>& a, sorted_impl){ sort(all(a)); }
+} sorted;
+struct reversed_impl{
+    template<class T> friend vector<T> operator|(vector<T> a, reversed_impl){ reverse(all(a)); return a; }
+    template<class T> friend void operator|=(vector<T>& a, reversed_impl){ reverse(all(a)); }
+} reversed;
 
 void Main(){
-    struct city { int x, y, z; };
-    int n;
-    cin >> n;
-    vector<city> a(n);
-    Graph g(n);
-    for(auto& [x, y, z] : a) cin >> x >> y >> z;
-    rep(n) rep(j, n) if(i != j)
-        g[i].push_back({j, abs(a[i].x - a[j].x) + abs(a[i].y - a[j].y) + max(0, a[j].z - a[i].z)});
-    vector dp(1 << n, vector(n, inf));
-    
+    int N; cin >> N;
+    vector<tuple<int, int, int>> city(N);
+    for(auto& [x, y, z] : city) cin >> x >> y >> z;
+    Graph g(N);
+    rep(N) rep(j, N) if(i != j){
+        auto [x1, y1, z1] = city[i];
+        auto [x2, y2, z2] = city[j];
+    }
 }
 
 signed main(){
-    cin.tie(0);
-    ios::sync_with_stdio(0);
+    cin.tie(0)->sync_with_stdio(false);
     cout << setprecision(10) << fixed;
     Main();
 }
